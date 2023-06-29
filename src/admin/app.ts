3e00c0ee13065fe6682664801a50d5f6dfb2911e
Eddie Villanueva <edvillan15@gmail.com>
2023-06-29 10:05:19 +0800

    Added API

---
+++ src/admin/app.ts
@@ -0,0 +1,20 @@
+export default {
+  config: {
+    // head: {
+    //   favicon: favicon,
+    // },
+    locales: [
+        'en',
+        'en-US',
+        'en-PH'
+    ],
+    translations: {
+      "en-US": {
+        'Auth.form.email.label': 'D8 Email',
+        'app.components.LeftMenu.navbrand.title': 'D8 Dashboard',
+      },
+    },
+  },
+  bootstrap() {},
+};
+
